ebb1706276e7912762844e93ec50754b21ebf48a
Laloeyudik <99laluwahyudi@gmail.com>
2025-02-02 09:32:03 +0800

    Menambahkan fitur baru

--- src/modules/users/helpers/link/generate-link.js
+++ src/modules/users/helpers/link/generate-link.js
@@ -1,7 +1,8 @@
 export function generateLink(req, token) {
   return new Promise((resolve, recject) => {
     try {
-      const links = `${req.protocol}://${req.get(
+      const protocol = req.get("x-forwarded-proto") || req.protocol;
+      const links = `${protocol}://${req.get(
         "host"
       )}/v1/login/verify?otl_token=${token}`;
       resolve(links);

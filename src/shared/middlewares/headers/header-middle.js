44344d6b5b1ef6857c83b599885819bc52791699
Laloeyudik <99laluwahyudi@gmail.com>
2025-02-05 13:02:30 +0800

    menambahkan  headers

--- src/shared/middlewares/headers/header-middle.js
+++ src/shared/middlewares/headers/header-middle.js
@@ -1,29 +1,22 @@
 import helmet from "helmet";
 
-const middHeader = helmet({
-  contentSecurityPolicy: {
-    directives: {
-      "script-src": ["'strict-dynamic'"],
-    },
-  },
-  xPoweredBy: false,
-  permissionsPolicy: {
-    features: {
-      geolocation: ["self"],
-      camera: [],
-      microphone: [],
-      fullscreen: ["*"],
-    },
-  },
-});
-
-const middPermissionsPolicy = (req, res, next) => {
+const middHeader = (req, res, next) => {
   const protocol = req.get("x-forwarded-proto") || req.protocol;
   const domain = `${protocol}://${req.get("host")}`;
+
   res.setHeader(
     "Permissions-Policy",
-    `geolocation=(self "${domain}"), microphone=()`
+    `geolocation=(self ${domain}), microphone=(), camera=(), fullscreen=*`
   );
-  next();
+
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'strict-dynamic'", `"${domain}"`],
+      },
+    },
+    xPoweredBy: false,
+  })(req, res, next);
 };
-export { middHeader, middPermissionsPolicy};
+
+export default middHeader;
